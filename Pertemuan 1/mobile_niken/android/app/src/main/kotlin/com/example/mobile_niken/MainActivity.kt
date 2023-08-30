e007c826a06a7a728b624b165c2c6af61ae377c2
nikenmn <nikenmaharani24@gmail.com>
2023-08-30 13:21:43 +0700

    first commit

---
+++ Pertemuan 1/mobile_niken/android/app/src/main/kotlin/com/example/mobile_niken/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mobile_niken
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Pertemuan 1/tugas1_niken/android/app/src/main/kotlin/com/example/tugas1_niken/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tugas1_niken
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
